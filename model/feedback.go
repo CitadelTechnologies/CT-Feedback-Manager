357c80f643562cc674dadf6a581ab4ea09733a1c
Axel Venet <kern046@gmail.com>
2017-12-16 14:52:57 +0100

    Implement model for feedback labels

--- model/feedback.go
+++ model/feedback.go
@@ -8,6 +8,8 @@ type Feedback struct {
   Author Author `json:"author"`
   Title string `json:"title"`
   Description string `json:"description"`
+  Labels Labels `json:"labels" bson:"_"`
+  LabelIds LabelIds `json:"_"`
   Comments Comments `json:"comments"`
   Status string `json:"status"`
   CreatedAt time.Time `json:"created_at"`
---
+++ model/label.go
@@ -0,0 +1,15 @@
+package model
+
+import(
+  "gopkg.in/mgo.v2/bson"
+)
+
+type(
+  Label struct {
+    Id bson.ObjectId `json:"id"`
+    Name string `json:"name"`
+    Color string `json:"color"`
+  }
+  Labels []Label
+  LabelIds []bson.ObjectId
+)
